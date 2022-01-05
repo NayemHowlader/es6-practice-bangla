fb6089729138ac673e4d11f951da62efe040d2c6
NayemHowlader <nayemhowlader77@gmail.com>
2022-01-05 11:16:16 +0600

    new update

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,7 @@
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("Condition is true");
+}else{
+    console.log("condition is false");
+}
---
+++ object-property.js
@@ -0,0 +1,12 @@
+const student = [
+    {id: 21, name: 'Omar Sunny'},
+    {id: 31, name: 'Manaaa'},
+    {id: 41, name: 'Mayori'},
+    {id: 71, name: 'Deepjol'},
+];
+
+const names = student.map(s => s.name);
+const ids = student.map(s => s.id);
+const bigger = student.filter(s => s.id > 40);
+const biggerOne = student.find(s => s.id > 40);
+console.log(biggerOne);
