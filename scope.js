83b179400c45c87fb05f48c999b3b23131088835
NayemHowlader <nayemhowlader77@gmail.com>
2022-01-05 12:39:12 +0600

    done

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 20
+
+function sum(first, second){
+    let result = first + second + bonus;
+   // console.log(bonus);
+
+   if(result > 9){
+       var mod = "happy";
+       mod =  "fishy"
+       mod =  "funcky"
+       mod =  "cracky"
+       //console.log(mod);
+   }
+   console.log(day);
+   let day = "friday";
+    return result;
+}
+
+const output = sum(10, 14);
+//console.log(bonus);
+//console.log(output);
